81cb43501b05c708e5febbf5542077739f35e41d
Magnific86 <klevtsov10012002@gmail.com>
2023-01-31 15:56:28 +0500

    a

--- components/auth/sign-up-form.js
+++ components/auth/sign-up-form.js
@@ -6,34 +6,47 @@ import Loader4LineIcon from "remixicon-react/Loader4LineIcon";
 import { Input, Select } from "antd";
 import pic from "../../public/assets/russia.png";
 import { DownSquareOutlined } from "@ant-design/icons";
+import PhoneInput, {
+  formatPhoneNumber,
+  formatPhoneNumberIntl,
+  isValidPhoneNumber,
+  isPossiblePhoneNumber,
+} from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import en from "react-phone-number-input/locale/en";
 
 const SignUpForm = ({ getVerifyCode, setPhone, loader, phone }) => {
+  console.log(phone);
   const [privacy, setPrivacy] = useState(true);
   const { t: tl } = useTranslation();
   const onFinish = (e) => {
     e.preventDefault();
-    getVerifyCode();
+    if (phone && isValidPhoneNumber && isPossiblePhoneNumber) {
+      getVerifyCode();
+      setPhone(formatPhoneNumber(phone));
+      alert(phone);
+    }
   };
   return (
     <div className="sign-up-form">
       <form onSubmit={onFinish}>
+        <PhoneInput
+          labels={en}
+          defaultCountry="RU"
+          value={phone}
+          onChange={(phone) => {
+            setPhone(() => {
+              setPhone(formatPhoneNumberIntl(phone));
+            });
+          }}
+        />
+
         {/*  <InputText
           onChange={(e) => setPhone(e.target.value)}
           // label="Phone number"
           placeholder=""
         /> */}
         {/* <Input.Group compact>
-          <Select defaultValue="Sign Up" style={{ width: "30%" }}>
-            <Option value="Sign Up">Sign Up</Option>
-            <Option value="Sign In">Sign In</Option>
-          </Select>
-          <AutoComplete
-            style={{ width: "70%" }}
-            placeholder="Email"
-            options={[{ value: "text 1" }, { value: "text 2" }]}
-          />
-        </Input.Group> */}
-        <Input.Group compact>
           <Select
             showArrow={true}
             size="large"
@@ -53,11 +66,11 @@ const SignUpForm = ({ getVerifyCode, setPhone, loader, phone }) => {
             </Option>
           </Select>
           <Input style={{ width: "70%", height: 70 }} type="tel" />
-        </Input.Group>
+        </Input.Group> */}
+
         <div style={{ height: 18 }} />
         <div className="privacy">
           <input type="checkbox" onChange={() => setPrivacy(!privacy)} />
-          {/* {tl("i agree to send SMS code.")} */}
           <Link href="/privacy-policy">
             <span>{tl("i agree to send SMS code. Privacy and Policy")}</span>
           </Link>
@@ -65,7 +78,7 @@ const SignUpForm = ({ getVerifyCode, setPhone, loader, phone }) => {
         <button
           data-loader={loader}
           disabled={!(!privacy && phone)}
-          type="submit"
+          // type="submit"
           // className="btn-auth"
         >
           {/* <Loader4LineIcon /> */}
