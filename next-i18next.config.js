842f4ffa71e3d4466b8b902c07ede47106cc864a
Magnific86 <klevtsov10012002@gmail.com>
2023-02-07 15:08:59 +0500

    a

---
+++ next-i18next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  i18n: {
+    locales: ["en", "ru"],
+    defaultLocale: "ru",
+  },
+  interpolation: {
+    prefix: "{",
+    suffix: "}",
+  },
+//   loadPath: "../public/locales/ru/translation.json",
+};
